5aa4a2ff96d24fb6f1d7adca7bb7bcc8e27f546b
msuzoagu <msuzoagu@gmail.com>
2015-12-21 11:50:58 -0500

    clarifies intended use of printer method

--- spec/conference_badges_spec.rb
+++ spec/conference_badges_spec.rb
@@ -25,12 +25,12 @@ describe 'conference_badges' do
                          ]}
 
 # This funny looking <<-TEXT thing is called heredoc. It's a multi-line string
-# delimiter that makes it really easy for us to write multi-line strings in the 
-# middle of code. The TEXT part is arbitrary too. 
-# It could easily have been <<-BLAHBLAH. The only requirement is that you use 
+# delimiter that makes it really easy for us to write multi-line strings in the
+# middle of code. The TEXT part is arbitrary too.
+# It could easily have been <<-BLAHBLAH. The only requirement is that you use
 # the same word to end the multi-line string.
 
-# For more info about heredocs, see this link: 
+# For more info about heredocs, see this link:
 # http://en.wikibooks.org/wiki/Ruby_Programming/Here_documents
 
   let(:badges_and_room_assignments) {<<-TEXT
@@ -84,10 +84,10 @@ TEXT
   describe '#printer' do
 
     # Question 4
+    # The method `printer` should output first the results of the batch_badge_creator method and then of the assign_rooms method to the screen - this way you can output
+    # the badges and room assignments one at a time.
+    # To make this test pass, make sure you are iterating through your badges and room assignments lists.
 
-    # For the purposes of this test, we are assuming that you decided to output
-    # the badges and room assignments one at a time. To make this test pass, make
-    # sure you are iterating through your badges and room assignments lists.
 
     it 'should puts the list of badges and room_assignments' do
       badges_and_room_assignments.each_line do |line|
